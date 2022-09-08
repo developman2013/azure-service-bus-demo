82ba3f796870adb7dc4b332540ce6d0cfbb81f97
Mikhail <developman2013@yandex.ru>
2022-09-08 17:45:15 +0300

    init

---
+++ AzureServiceBusSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+
+namespace AzureServiceBusSender;
+
+public class Program
+{
+    private static readonly string _connectionString = "Endpoint=sb://...";
+    private static readonly string _topic = "direct-messages-topic";
+
+    public static async Task Main()
+    {
+        var sender = new ServiceBusClient(_connectionString).CreateSender(_topic);
+
+        Console.WriteLine("Your username:");
+        var username = Console.ReadLine();
+
+        Console.Clear();
+        Console.WriteLine("Type your message and press Enter:");
+        while (true)
+        {
+            var message = Console.ReadLine();
+            var messageJson = JsonConvert.SerializeObject(new MessageModel
+            {
+                 Message = message,
+                 Username = username
+            });
+            await sender.SendMessageAsync(new ServiceBusMessage(messageJson));
+        }
+    }
+}
